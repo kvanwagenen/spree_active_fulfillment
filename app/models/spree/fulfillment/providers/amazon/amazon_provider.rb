025869e919a3d4e7bf937eb856e50f2f13cd568c
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-05-26 13:18:24 -0600

    Nest modules at consistent level

--- app/models/spree/fulfillment/providers/amazon/amazon_provider.rb
+++ app/models/spree/fulfillment/providers/amazon/amazon_provider.rb
@@ -1,43 +1,41 @@
-module Spree
-  module Fulfillment::Providers::Amazon
-    class AmazonProvider < Spree::Fulfillment::Provider
+module Spree::Fulfillment::Providers::Amazon
+  class Provider < Spree::Fulfillment::Provider
 
-      def initialize
-        @fulfillment_preview_cache = FulFillmentPreviewCache.new
-      end
-      
-      def services
-        [:standard, :expedited, :priority]
-      end
-
-      def can_fulfill?(package)
-        raise NotImplementedError, "#can_fulfill? is not yet supported by #{self.class.name}."
-      end
-
-      def estimate_cost(package, service)
-        fulfillment_preview(package, service).cost
-      end
-
-      def estimate_delivery_date(package, service, ship_date)
-        fulfillment_preview(package, service).delivery_date_estimate
-      end
-
-      def fulfill(shipment)
-        raise NotImplementedError, "#fulfill is not yet supported by #{self.class.name}."
-      end
-
-      def cancel_fulfillment(fulfillment)
-        raise NotImplementedError, "#cancel_fulfillment is not yet supported by #{self.class.name}."
-      end
-
-      private
-
-      attr_accessor :fulfillment_preview_cache
-
-      def fulfillment_preview(package, service)
-        preview = fulfillment_preview_cache.get(package, service)
-      end
-
+    def initialize
+      @fulfillment_preview_cache = FulFillmentPreviewCache.new
     end
+    
+    def services
+      [:standard, :expedited, :priority]
+    end
+
+    def can_fulfill?(package)
+      raise NotImplementedError, "#can_fulfill? is not yet supported by #{self.class.name}."
+    end
+
+    def estimate_cost(package, service)
+      fulfillment_preview(package, service).cost
+    end
+
+    def estimate_delivery_date(package, service, ship_date)
+      fulfillment_preview(package, service).delivery_date_estimate
+    end
+
+    def fulfill(shipment)
+      raise NotImplementedError, "#fulfill is not yet supported by #{self.class.name}."
+    end
+
+    def cancel_fulfillment(fulfillment)
+      raise NotImplementedError, "#cancel_fulfillment is not yet supported by #{self.class.name}."
+    end
+
+    private
+
+    attr_accessor :fulfillment_preview_cache
+
+    def fulfillment_preview(package, service)
+      preview = fulfillment_preview_cache.get(package, service)
+    end
+
   end
 end
