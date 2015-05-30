4fefd8c031413e0ff934b624189cdd2b4c16ccbb
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-05-29 18:26:39 -0600

    Fix fulfillment config

--- app/models/spree/fulfillment/configuration.rb
+++ app/models/spree/fulfillment/configuration.rb
@@ -6,9 +6,10 @@ module Spree
       preference :aws_access_key_id, :string
       preference :aws_secret_access_key, :string
       preference :amazon_stock_location_id, :integer
+      preference :load_most_recent_reports, :boolean, default: false
 
       def amazon_provider
-        @amazon_provider ||= Providers::Amazon::AmazonProvider.new
+        @amazon_provider ||= Providers::Amazon::Provider.new
       end
     end
   end
