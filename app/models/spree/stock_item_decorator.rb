3e271d9b05cdfa855aa2ea8478c98af654b2a40f
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-06-02 11:37:45 -0600

    Fix stock item find_by_variant

--- app/models/spree/stock_item_decorator.rb
+++ app/models/spree/stock_item_decorator.rb
@@ -1,5 +1,5 @@
 Spree::StockItem.class_eval do
   def self.find_by_variant_sku(sku)
-    join(:variant).where("spree_variants.sku = ?", sku).take
+    joins(:variant).where("spree_variants.sku = ?", sku).take
   end
 end
