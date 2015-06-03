88e29178dcdfe5b6fedcc3fd667653ace811435f
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-06-02 19:37:46 -0600

    Work some on create fulfillment order request

--- app/models/spree/fulfillment/providers/amazon/create_fulfillment_order_request.rb
+++ app/models/spree/fulfillment/providers/amazon/create_fulfillment_order_request.rb
@@ -1,5 +1,19 @@
 module Spree::Fulfillment::Providers::Amazon
   class CreateFulfillmentOrderRequest < PeddlerRequest
+    def fulfillment_order(package, service)
+      FulfillmentOrder.new(parsed_response)
+    end
 
+    protected
+
+    def client_class
+      ::MWS::FulfillmentOutboundShipment::Client
+    end
+
+    private
+
+    def parsed_response
+      client.create_fulfillment_order()
+    end
   end
 end
